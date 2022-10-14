4d4eacb4a78be1333dd43be0ba9ab8abd7171e3e
rita <mattarr@educ.college-montreal.qc.ca>
2022-10-14 08:38:52 -0400

    jeu de devinette (2)

--- Devinette.py
+++ Devinette.py
@@ -14,7 +14,7 @@ def nombre():
     cette fonction defini les nombres maximales et minimales
     """
     global minimum, maximum
-    minimum = int(input("écrivez un nombre minimal:" ))
+    minimum = int(input("écrivez un nombre minimal:"))
     maximum = int(input("écrivez un nombre maximal"))
     print(f"vos numéro était sauvgarder. Maintenant essay de deviner le nombre que j'ai choisi entre {minimum} et {maximum}")
 
@@ -26,13 +26,13 @@ def jeu_devinette():
     ce dernier détermine le nombre d'essaies, aide le joueur a deviner le nombre et lui offre de rejouer
     """
     nombre()
-    chiffre = random.randint(minimum,maximum)
+    chiffre = random.randint(minimum, maximum)
     chances_counter = 1
     chances = int(input("deviner le nombre:"))
     while chances != chiffre:
         if chances > chiffre:
             print("votre nombre est plus grand que le nombre choisi ")
-            chances_counter =chances_counter + 1
+            chances_counter = chances_counter + 1
             chances = int(input("deviner un autre nombre "))
 
         else:
@@ -40,7 +40,7 @@ def jeu_devinette():
             chances_counter = chances_counter + 1
             chances = int(input("deviner un autre nombre"))
 
-    print (f"exellent, vous avez reussi a deviner le bon nombre qui etait {chances}")
+    print(f"exellent, vous avez reussi a deviner le bon nombre qui etait {chances}")
     print(f"vous avez reussit a devibe le nombre dans {chances_counter}")
     rejouer = str(input("voulez vous faire un autre tour? (y/n"))
     if rejouer == "y":
